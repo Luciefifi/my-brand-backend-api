cf7baaeb028e885ac23ffaeb4dde3b7eb28f51a4
Luciefifi <niyomutonilucie@gmail.com>
2023-01-13 09:46:39 +0200

    feat(allMessages): -create get all messages endpoint - create get request - modify messageRoute.js file [Finishes #005]

--- Controllers/messageController.js
+++ Controllers/messageController.js
@@ -1,7 +1,7 @@
 import Message from "../Models/messageModel";
 
 class messageController {
-
+//create a contact message
     static async createMessage(req, res) {
         try {
           const message = new Message({
@@ -18,10 +18,21 @@ class messageController {
         }
       }
 
+      //get single message
       static async getSingleMessage(req,res) {
         try {
        const message=await Message.findById(req.param.id);
        res.status(200).json(message)
+        }catch (error){
+            res.status(404).json(error.message);
+        }
+    }
+
+//get all messages
+    static async getAllMessages(req,res) {
+        try {
+       const messages=await Message.find();
+       res.status(200).json(messages)
         }catch (error){
             res.status(404).json(error.message);
         }
--- Routes/messageRoute.js
+++ Routes/messageRoute.js
@@ -5,5 +5,6 @@ const messageRoute =express.Router();
 
 messageRoute.post("/createMessage",messageController.createMessage);
 messageRoute.get("/getSingleMessage/:id",messageController.getSingleMessage);
+messageRoute.get("/getAllMessages",messageController.getAllMessages)
 
 export default messageRoute;
