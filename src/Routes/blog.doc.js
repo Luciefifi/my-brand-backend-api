3b3b55d606b319f2cd18dfff5e21193fe527db5a
Luciefifi <niyomutonilucie@gmail.com>
2023-01-25 14:32:39 +0300

    feat(get a blog by id swagger doc): -modify blogController.js file -modify blog.doc.js file -[finishes #026]

--- src/Routes/blog.doc.js
+++ src/Routes/blog.doc.js
@@ -1,4 +1,5 @@
 
+//get all blogs swagger documentation
 const listOfAllBlogs = {
     
     tags:["Blogs"],
@@ -28,6 +29,50 @@ const listOfAllBlogs = {
     },
 };
 
+//get single blog by id swagger documentation
+const getSingleBlog ={
+    tags:["Blogs"],
+    summary:"get user by path id",
+    description:"get single blog by id",
+    parameters:[
+        {
+            name: "id",
+            in : "path",
+            description: "id of the user" ,
+            type: "string",
+            example: "hfbjsd2345njndfjhcbe3"
+        },
+    ],
+    responses:{
+        200:{
+            description:"OK",
+            content:{
+                "application/json":{
+                    schema:{
+                        type:"object",
+                        example:{
+                            
+                            _id: "63ccde6635bde581af696708",
+                            title: "dancing",
+                            description: "dancing is a very good physical exercise for the dancer",
+                            image: "http://localhost:5000/images/1674370662518pexels-harry-dona-2338407.jpg",
+                            blogBody: "dancing makes most of the dancer feel good\ndancing makes most of the dancer feel gooddancing makes most of the dancer feel good\ndancing makes most of the dancer feel gooddancing makes most of the dancer feel good",
+                            __v: 0
+                          
+                    },
+  
+                    },
+                },
+            },
+        },
+        404:{
+            description:"blog not found"
+        }
+    },
+  
+  }
+  
+
 
 
 
@@ -41,7 +86,10 @@ const listOfAllBlogs = {
 const blogRouteDoc ={
     "/api/getAllBlogs": {
         get:listOfAllBlogs
-    }
+    },
+    "/api/getSingleBlog/{id}":{
+        get:getSingleBlog
+      }
    
 };
 
