3d5ca86440a8b57797d4331264ac40c56d5071c1
Luciefifi <niyomutonilucie@gmail.com>
2023-01-26 10:55:33 +0300

    feat(login swagger doc): -modify user.doc.js -[finishes#034]

--- src/Routes/user.doc.js
+++ src/Routes/user.doc.js
@@ -56,11 +56,57 @@ const registerUser = {
   
   
   };
+//logging in a user swagger documentation
+  const login = {
+    tags:["Users"],
+    description: "login user",
+    requestBody:{
+        content:{
+            "Application/json":{
+                schema:{
+                    type:"object",
+                    properties:{
+                        email:{
+                            type:"email",
+                            description:"email of the user",
+                            example:"lavender@gmail.com"
+                        },
+                        password:{
+                            type:"string",
+                            description:"password of the user",
+                            example:"12345678",
+                        },
+                        
+                    },
+                },
+            },
+  
+        },
+    },
+  responses:{
+        200:{
+            description:"OK",
+            content:{
+                "application/json":{
+                    schema:{
+                        type:"object",
+  
+                    },
+                },
+            },
+        },
+    },
+  
+  
+  };
 
 
   const userRouteDoc = {
     "/api/createUser": {
         post:registerUser
+    },
+    "/api/login": {
+        post:login
     },
 
   }
